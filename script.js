42e2025aafb0072f4ab0259ef6ab2c27e094918f
sdcnai-IDM-2251 <sdcnai@g.rit.edu>
2026-09-17 09:44:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -352,8 +352,8 @@ function draw() {
 function drawBackgroundSky() {
   const gradient = ctx.createLinearGradient(0, 0, 0, window.innerHeight);
   if (settings.mode === "dark") {
-    gradient.addColorStop(1, "#27507F");
-    gradient.addColorStop(0, "#58A8D8");
+    gradient.addColorStop(1, "#6C5B7B");
+    gradient.addColorStop(0, "#355C7D");
   } else {
     gradient.addColorStop(1, "#F8BA85");
     gradient.addColorStop(0, "#FFC28E");
